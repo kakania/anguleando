7509ed7ef8f01843581489e97252021aae487f80
servando <jofrasa@hotmail.com>
2015-09-07 22:44:21 +0100

    Added APM.Client2 - From course from D. Kubata 'Angula - LoB App'

---
+++ APM.Client2/common/services/productResource.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("common.services")
+                      .factory("productResource",
+                               ["$resource",
+                                "appSettings",
+                                productResource]);
+
+    function productResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/products/:productId", null,
+            {
+                'update':{method:'PUT'}
+            });
+    }
+
+}());
